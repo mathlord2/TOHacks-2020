9746bdecd36706ce901f66b8408cb1895b972b2d
Eric Xiao <42785168+mathlord2@users.noreply.github.com>
2020-05-02 14:00:09 -0400

    Edits

---
+++ frontend/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { mainPage } from "./components/mainPage.svelte"
+</script>
+
+<mainPage>
+</mainPage>
+
+<style>
+	
+</style>
---
+++ frontend/src/components/mainPage.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {onMount} from 'svelte';
+    export let mainPage;
+</script>
+
+<div class="mainPage">
+    <h1>Upload lung image here:</h1>
+    <button>Upload <i class="fa fa-upload"></i></button>
+</div>
+
+<style>
+.mainPage {
+    width: 100%;
+    text-align: center;
+}
+</style>
